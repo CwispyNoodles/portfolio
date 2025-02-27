b328db3249e160c25abf86dd28f9d0a0f5cfd412
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2025-02-26 21:16:15 -0500

    installed react three/drei

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,22 @@
 import styles from './Hero.module.css'
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame} from '@react-three/fiber';
+import { useRef } from 'react';
+
+function AnimatedBox() {
+    const boxRef = useRef();
+
+    useFrame(() => {
+        boxRef.current.rotation.x += 0.005;
+        boxRef.current.rotation.y += 0.005;
+        boxRef.current.rotation.z += 0.005;
+    });
+    return(
+        <mesh ref={boxRef}>
+            <boxGeometry args={[2,2,2]}/>
+            <meshStandardMaterial color={0x00bfff}/>
+        </mesh>
+    );
+}
 
 function Hero() {
     return(
@@ -16,13 +33,9 @@ function Hero() {
         //     </div>
         // </div>
         <div id='canvas-container'>
-            <Canvas camera={{position: [2,2,2]}}>
+            <Canvas >
                 <mesh position={[-2, 2, -1]} rotation={[0,0,Math.PI]} scale={[2,0.5, 2]}>
-                    {/* <sphereGeometry args={[3, 32, 32]}/> */}
-                    {/* <boxGeometry args={[2,3,2]}/> */}
-                    <torusKnotGeometry args={[1.7, 0.3, 256, 256]}/>
-                    {/* <meshPhongMaterial color={0x00bfff}/> */}
-                    <meshToonMaterial color={0x00bfff}/>
+                    <AnimatedBox/>
                     <directionalLight position={[2,5,1]}/>
                 </mesh>
             
